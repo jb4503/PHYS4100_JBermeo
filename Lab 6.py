2c67fcf195849ea5f2c4b7fbcf1484821558b727
JB <66484039+jb4503@users.noreply.github.com>
2021-03-09 17:23:31 -0500

    Add files via upload
    Fixed a silly issue,
    Not sure if my values are very accurate.

--- Lab 6.py
+++ Lab 6.py
@@ -29,8 +29,6 @@ def main():
     # lines like before
     c = c_e*((w ** 2) * m) / (2 * hbar ** 2)
 
-    # To find the roots
-
     # First function
     def y1(e):
         return np.tan(np.sqrt(c * e))
@@ -43,25 +41,15 @@ def main():
     def y3(e):
         return -np.sqrt(e / (V - e))
 
-    # For bisection purposes
-    def f1(x):
-        return y1(x) - y2(x)
-
-    def f2(x):
-        return y1(x) - y3(x)
-
-    accuracy = 1e-3  # in Ev
-
+    # Finds the roots to a desired accuracy
     def roots(f, x1, x2, acc):
         def midpoint(x, y):
             return (x + y) / 2
-
         def have_same_sign(x, y):
             if x < 0 and y < 0 or x > 0 and y > 0:
                 return True
             else:
                 return False
-
         while abs(x1 - x2) > acc:
             x = midpoint(x1, x2)
             if have_same_sign(f(x1), f(x)):
@@ -73,26 +61,38 @@ def main():
 
         return midpoint(x1, x2)
 
+    def f1(x):
+        return y1(x) - y2(x)
+
+    def f2(x):
+        return y1(x) - y3(x)
+
     # For the different values of E
     E = np.linspace(.05, 19.95, 100)
-    y1 = y1(E)
-    y2 = y2(E)
-    y3 = y3(E)
+
+    # Initially I had used the names y1,y2 and y3
+    # This was a fatal mistake and caused call issues down below
+    # Now I have learned never to reuse variable and functions names
+    Y1 = y1(E)
+    Y2 = y2(E)
+    Y3 = y3(E)
     # Plot the values of E
-    plt.plot(E,y1, '-', label='tangent')
-    plt.plot(E, y2, '-', label='positive values')
-    plt.plot(E, y3, '-', label='negative values')
+    plt.plot(E,Y1, '-', label='tangent')
+    plt.plot(E, Y2, '-', label='positive values')
+    plt.plot(E, Y3, '-', label='negative values')
     plt.legend()
     plt.xlabel('E in electronVolts')
     plt.show()
 
+
     # Finding the first 6 values for energy levels
-    print('E0 = ', roots(f1, 0.02, 0.75, accuracy))
-    print('E1 = ', roots(f2, 1, 1.5, accuracy))
-    print('E2 = ', roots(f1, 2.5, 4.5, accuracy))
-    print('E3 = ', roots(f2, 5, 6, accuracy))
-    print('E4 = ', roots(f1, 7.5, 9, accuracy))
-    print('E5 = ', roots(f2, 10, 12, accuracy))
+    accuracy = 1e-5  # in Ev
+    print('E0 = ', roots(f1, 0.02, 0.5, accuracy))
+    print('E1 = ', roots(f2, 0.75, 1.5, accuracy))
+    print('E2 = ', roots(f1, 2.5, 3, accuracy))
+    print('E3 = ', roots(f2, 5, 8, accuracy))
+    print('E4 = ', roots(f1, 7, 9, accuracy))
+    print('E5 = ', roots(f2, 10, 12.5, accuracy))
 
     pr.disable()
     # pr.print_stats(sort='time')
