07e80258ef94591cd5e073200ec9b2a81c16c708
JB <66484039+jb4503@users.noreply.github.com>
2021-05-10 15:48:00 -0400

    Add files via upload
    Commit on Mac, need to test the code on a windows

--- Lab_10.py
+++ Lab_10.py
@@ -2,7 +2,8 @@ import time
 import cProfile
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from numpy import abs, exp, linspace, array, zeros, empty, real, imag
+from numpy import abs, exp, linspace, array, zeros, empty, real, imag, dot, delete
+from numpy.linalg import solve, pinv
 from scipy.linalg import solve_banded
 
 
@@ -11,10 +12,44 @@ ELECTRON_MASS = 9.109e-31
 L = 1e-8  # m
 TIME_STEP = 1e-18  # seconds
 SIGMA = 1e-10  # m
-K = 5e10  # 1/m
+KAPPA = 5e10  # 1/m
 H_BAR = 6.626e-34
 N_SLICES = 1000  # number of spatial slices
 A_DIST = L / N_SLICES
+
+
+def psi_function(x=1, length=1e-8, sigma=1e-10, kappa=1e10):
+    """
+    Returns the first value of psi at t=0 for all x values
+    :param x:
+    :param length: length of the box, in meters
+    :param sigma:
+    :param kappa:
+    :return:
+    """
+    x_0 = length / 2
+    num = (x - x_0) ** 2
+    den = 2 * (sigma ** 2)
+    psi_0 = exp(-(num / den)) * exp(1j * kappa * x)
+    return psi_0
+
+
+def TriDiag_Matrix(A, a1, a2,N=100):
+    """
+    Creates a Tridiag Matrix A
+    given diagonal element a1
+    and banded elements a2
+    """
+    for i in range(N):
+        A[i, i] = a1
+        if i == 0:
+            A[i, i+1] = a2
+        elif i == N-1:
+            A[i, i-1] = a2
+        else:
+            A[i, i+1] = a2
+            A[i, i-1] = a2
+    return A
 
 
 def main():
@@ -26,19 +61,14 @@ def main():
     # Initial parameters
     # Constants
 
-    x0 = L / 2
-
-    a1 = 1 + 1j * TIME_STEP * H_BAR / (2 * ELECTRON_MASS * A_DIST ** 2)
+    a1 = 1 + TIME_STEP*((1j * H_BAR) /(2 * ELECTRON_MASS * (A_DIST**2)))
     a2 = - TIME_STEP * H_BAR * 1j / (4 * ELECTRON_MASS * A_DIST ** 2)
     b1 = 1 - 1j * TIME_STEP * H_BAR / (2 * ELECTRON_MASS * A_DIST ** 2)
     b2 = - a2
 
-    def psi_0(x):
-        return exp(-(x - x0) ** 2 / (2 * SIGMA ** 2)) * exp(1j * K * x)
-
     # initial conditions
-    x_points = linspace(0, L, N_SLICES + 1)
-    psi = array(list(map(psi_0, x_points)), complex)
+    x_points = linspace(0, L, N_SLICES + 1, endpoint=False)
+    psi = array(list(map(psi_function, x_points)), complex)
     psi[0] = psi[N_SLICES - 1] = 0
 
     # Create the matrix A
@@ -48,7 +78,6 @@ def main():
     A2[1, :] = a1
     A2[2, 0: N_SLICES] = a2
     A2[2, N_SLICES] = 0
-
     # store the wave function at each time step in a list
     solution = [psi]
     for i in range(500):
@@ -56,17 +85,41 @@ def main():
         psi = solve_banded((1, 1), A2, psi)
         solution.append(psi)
 
-    plt.plot(x_points, real(solution[4]))
-    #plt.plot(x_points, real(solution[49]) ** 2)
-    #plt.plot(x_points, real(solution[250]) ** 2)
+    plt.plot(x_points, real(solution[25]))
+    plt.plot(x_points, real(solution[49]) ** 2)
+    plt.plot(x_points, real(solution[250]) ** 2)
     plt.xlabel("x (m)")
     plt.ylabel("$\psi(x)$")
     plt.show()
 
-    # creating a blank window
-    # for the animation
-    psi_2 = []
-    psi_2.append(psi_0(x_points))
+    # Animation
+
+    def init():
+        line.set_data(x_points, psi_function(xpoints, L, SIGMA, KAPPA))
+        return line,
+
+    A_2 = zeros([N_SLICES, N_SLICES], dtype=complex)
+    A_2 = TriDiag_Matrix(A_2, a1, a2)
+    B = zeros([N_SLICES, N_SLICES], dtype=complex)
+    B = TriDiag_Matrix(B, b1, b2, N_SLICES)
+
+    xpoints = (L / N_SLICES) * linspace(0, N_SLICES, num=N_SLICES + 1, endpoint=False)
+    xpoints = delete(xpoints, 0)
+
+    def animate(i):
+        v = dot(B, psi[i])
+        psi_2.append(solve(A_2, v))
+        x = xpoints
+        y = psi[i]
+        i += 1
+        line.set_data(x, y)
+        return line,
+
+        # calling the animation function
+        # creating a blank window
+        # for the animation
+
+    psi_2 = [psi_function(xpoints, L, SIGMA, KAPPA)]
     fig = plt.figure()
     axis = plt.axes(xlim=(-1e-9, 11e-9), ylim=(-1, 1))
     axis.set_xlabel('x')
@@ -74,40 +127,12 @@ def main():
     axis.set_title('Crank Nicolson Wave Function')
     line, = axis.plot([], [], lw=.5)
 
-    # what will our line dataset
-    # contain?
-    def init():
-
-        line.set_data(x_points, psi_0(x_points))
-        return line,
-
-    # animation function
-    solution = [psi_2]
-    def animate(i):
-        solution = [psi_2]
-        for i in range(500):
-            psi_2[1: N_SLICES] = b1 * psi_2[1: N_SLICES] + b2 * (psi_2[2:] + psi[0: N_SLICES - 1])
-            psi_2 = solve_banded((1, 1), A2, psi)
-            solution.append(psi_2)
-        # x, y values to be plotted
-        x = x_points
-        y = solution[i]
-        # appending values to the previously
-        i += 2500
-        line.set_data(x, y)
-
-        return line,
-
-        # calling the animation function
-
-    anim = animation.FuncAnimation(fig, animate,
-                                   init_func=init,
-                                   frames=500,
-                                   interval=20,
-                                   blit=True)
-
+    animate_wave = animation.FuncAnimation(
+        fig, animate, init_func=init, frames=100000, interval=500, blit=True,
+        save_count=50)
+    my_writer = animation.PillowWriter(fps=30, codec='libx264', bitrate=2)
     plt.show()
-    anim.save('Lab10GIF.gif', writer=animation.PillowWriter(fps=30))
+
 
     pr.disable()
     # pr.print_stats(sort='time')
